0bcf1bf311e1fc30ee1a84b6264c7b76ede0ef38
Rigwarl <Avips@mail.ru>
2016-03-17 17:23:50 +0300

    now set init value with setValue()

--- app/js/es6/elements/score.js
+++ app/js/es6/elements/score.js
@@ -4,7 +4,8 @@ export default class extends Element {
   constructor(config) {
     super();
     this.config = config;
-    this.el = new createjs.Text(`Поймано 0/${this.config.toCatch}`, '35px Kremlin', '#303030');
+    this.el = new createjs.Text('', '35px Kremlin', '#303030');
+    this.setValue(0);
     this.set();
     this.addToResize();
   }
