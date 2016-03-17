8bedfbdb37e57746882b2fbc027ce7cc78320900
Rigwarl <Avips@mail.ru>
2016-03-17 16:44:00 +0300

    write config to prop

--- app/js/es6/elements/agava.js
+++ app/js/es6/elements/agava.js
@@ -3,21 +3,22 @@ import Element from './element.js';
 export default class extends Element {
   constructor(sprite, config) {
     super();
+    this.config = config;
     this.el = new createjs.Sprite(sprite);
     this.bounds = this.el.getBounds();
     this.frames = sprite.getNumFrames();
-    this.set(config);
-    this.bindEvents(config);
+    this.set();
+    this.bindEvents();
   }
-  set(config) {
+  set() {
     this.el.gotoAndStop(0);
     this.el.set({
       x: Math.random() * this.gb.cw,
       regX: this.bounds.width / 2,
     });
-    setTimeout(() => this.el.play(), Math.random() * config.agavaDelay);
+    setTimeout(() => this.el.play(), Math.random() * this.config.agavaDelay);
   }
-  bindEvents(config) {
-    this.el.addEventListener('animationend', () => this.set(config));
+  bindEvents() {
+    this.el.addEventListener('animationend', () => this.set());
   }
 }
