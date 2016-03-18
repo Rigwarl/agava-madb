df681dcf6d02768c0e32cf89fa141dc8693601f7
Rigwarl <Avips@mail.ru>
2016-03-18 15:28:42 +0300

    add light

--- app/js/es6/elements/background.js
+++ app/js/es6/elements/background.js
@@ -3,7 +3,11 @@ import Element from './element.js';
 export default class extends Element {
   constructor(queue) {
     super();
-    this.el = new createjs.Bitmap(queue.getResult('bg'));
+    this.el = new createjs.Container();
+    this.el.addChild(
+      new createjs.Bitmap(queue.getResult('bg')),
+      new createjs.Bitmap(queue.getResult('light'))
+    );
     this.set();
     this.addToResize();
   }
